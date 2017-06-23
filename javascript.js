0d59f82964a1083417250206c6e584881b7761be
Andy Bonner <abonner@duke.edu>
2017-06-22 21:22:36 -0400

    stuff

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 
-var searchTerm = "";
+var searchDiv = "";
 var numberRecords;
 var startYear;
 var endYear;
@@ -7,12 +7,12 @@ var endYear;
 
 
 $("#searchButton").on("click", function () {
-  searchTerm = $("#searchTerm").val();
+  searchDiv = $("#searchDiv").val();
   numberRecords = $("#numberRecords").val();
   startYear = $("#startYear").val();
   endYear = $("#endYear").val();
 
-  var queryURL = "http://developer.nytimes.com/proxy/https/api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8bcf9cc14588480da94b4e7b37f44aa6&q=" + searchTerm + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231";
+  var queryURL = "http://developer.nytimes.com/proxy/https/api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8bcf9cc14588480da94b4e7b37f44aa6&q=" + searchDiv + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231";
 
   $.ajax({
     type: "GET",
@@ -23,10 +23,16 @@ $("#searchButton").on("click", function () {
       var articleArray = response.docs;
 
       for (var i = 0; i < numberRecords; i++) {
+        var thisArticle = articleArray[i];
         var articleCount = 1;
         var article = $("<div>");
         var articleNum = $("<div>"); // let front-end take care of styling
-        articleNum.text(articleCount); 
+        articleNum.text(articleCount);
+        var headLine = $("<div>");
+        headLine.append()
+
+
+        articleCount++;
       }
 
 
